6a575ac72ab91b34d8a5bb0c821d03aed9ba4525
shundahoy <shundahoy@gmail.com>
2022-08-27 10:58:16 +0900

    updete

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Product, FooterBanner, HeroBanner } from "../components";
-import { client } from "../lib/client";
-const Home = ({ products, bannerData }) => {
-  return (
-    <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      <div className="products-heading">
-        <h2>Beset Selling Products</h2>
-        <p>Speaker of many varidatation</p>
-      </div>
-      <div className="products-container">
-        {products?.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </div>
 
-      <FooterBanner />
-    </>
-  );
-};
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
